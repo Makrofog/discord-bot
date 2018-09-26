31a0a2c255ba5be7de64097b0ce37137b8938e8f
Makrofog <42889197+Makrofog@users.noreply.github.com>
2018-09-26 19:11:17 +0200

    Update macrophagev2.py

--- macrophagev2.py
+++ macrophagev2.py
@@ -7,13 +7,17 @@ import asyncio
 import time
 import os
 
+
+
+
+
 BOT_PREFIX= "."
 TOKEN = "NDUwNDM3NDc0OTUxNjI2NzUz.Dmqusw.TI9ft6al2xLlhk5aBVA7ETEqQ60"
 
-client = Bot (command_prefix=BOT_PREFIX) 
+client = Bot (command_prefix=BOT_PREFIX)
 
-@client.command(name='8ball',
-				description="madry bot",
+@client.command(name='czy',
+				description="odpowiada",
 				brief="odpowiada na pytania zamkniete",
 				pass_context=True)
 async def eight_ball(context):
@@ -23,36 +27,71 @@ async def eight_ball(context):
 		'Może',
 		':thinking:',
 	]
-	await client.say(random.choice(possible_responses)+ "," + context.message.author.mention)
-	
+	await client.say(random.choice(possible_responses))
+
 @client.event
 async def on_ready():
 	await client.change_presence(game=Game(name='prefix [.]'))
 	print("logged in as" + client.user.name)
-	
-@client.command(pass_context=True)
+
+@client.command(name='clear',
+				description="czyszczenie",
+				brief="wprowadź daną liczbę by wyczyśić tyle komentarzy ",
+				pass_context=True)
 async def clear(ctx, amount=100):
 	channel = ctx.message.channel
-	messages = [] 
+	messages = []
 	async for message in client.logs_from(channel, limit=int(amount)):
 		messages.append(message)
 	await client.delete_messages(messages)
 
-@client.command()
-async def embed():
+
+@client.command(name='info',
+				description="informacje",
+				brief="informacje na temat ról (nie nie z tego serwera krzysiek)",
+				pass_context=True)
+async def info():
 	embed = discord.Embed (
-		title= 'title',
-		description='description',
-		colour= discord.Colour.blue()
+		title= 'Informacje o rolach ',
+		description= 'Role dla community ',
+		color = discord.Color.blue()
 	)
 
-	embed.set_footer(text='lul')
-	embed.set_image(url='https://cdn.discordapp.com/attachments/485045416757428244/485103687635828737/nerfglogopodstawowe.png')
-	embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/485045416757428244/485103687635828737/nerfglogopodstawowe.png')
-	embed.set_author(name='Makro', 
-	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/485103687635828737/nerfglogopodstawowe.png')
+	embed.set_footer(text='W razie problemow prosimy o kontakt z administracja')
+	embed.set_author(name='Makro',
+	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/487981250108194816/zmxn.png')
+	embed.add_field(name="member", value="Rola dla osob ktore osiagnely lvl 5", inline=False)
+	embed.add_field(name="Active member", value="Rola dla aktywnych osob (+10lvl)", inline=False)
+	embed.add_field(name="verifed member", value="rola przyznawana przez administracje dla zaufanych osob", inline=False)
 
 	await client.say(embed=embed)
+
+@client.command(name='regulamin',
+				description="regulamin",
+				brief="aktualny regulamin serwera (nie też nie tego serwera)",
+				pass_context=True)
+async def regulamin():
+	embed = discord.Embed (
+		title= 'REGULAMIN',
+		description= 'Serwer jest publicznym serwerem z tego powodu prosimy zachowac kulture osobista',
+		color = discord.Color.blue()
+	)
+
+	embed.set_footer(text='W razie problemow prosimy o kontakt z administracja')
+	embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/485045416757428244/487981245826072577/XD.png')
+	embed.set_author(name='Makro',
+	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/487981250108194816/zmxn.png')
+	embed.add_field(name="zakaz obrazania", value="Prosimy nie obrazac innych uzytkownikow", inline=False)
+	embed.add_field(name="zakaz kontentu NSFW", value="Prosimy nie udostepniac tresci pornograficznych itp, itd", inline=False)
+	embed.add_field(name="zakaz reklamy", value="Prosimy nie reklamowac sie na serwerze", inline=False)
+
+	await client.say(embed=embed)
+
+
+
+
+
+
 
 
 client.run(TOKEN)

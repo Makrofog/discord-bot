7ceb858de75e9e51d821629815a79deff2b6897f
Makrofog <42889197+Makrofog@users.noreply.github.com>
2018-10-07 16:04:39 +0200

    Update macrophagev2.py

--- macrophagev2.py
+++ macrophagev2.py
@@ -46,46 +46,46 @@ async def clear(ctx, amount=100):
 	await client.delete_messages(messages)
 
 
-#@client.command(name='info',
-#				description="informacje",
-#				brief="informacje na temat ról (nie nie z tego serwera krzysiek)",
-#				pass_context=True)
-#async def info():
-	#embed = discord.Embed (
-	#	title= 'Informacje o rolach ',
-	#	description= 'Role dla community ',
-	#	color = discord.Color.blue()
-	#)
+@client.command(name='info',
+				description="informacje",
+				brief="informacje na temat ról (nie nie z tego serwera krzysiek)",
+				pass_context=True)
+async def info():
+	embed = discord.Embed (
+		title= 'Informacje o rolach ',
+		description= 'Role dla community ',
+		color = discord.Color.blue()
+	)
 
-#	embed.set_footer(text='W razie problemow prosimy o kontakt z administracja')
-#	embed.set_author(name='Makro',
-#	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/487981250108194816/zmxn.png')
-#	embed.add_field(name="member", value="Rola dla osob ktore osiagnely lvl 5", inline=False)
-#	embed.add_field(name="Active member", value="Rola dla aktywnych osob (+10lvl)", inline=False)
-#	embed.add_field(name="verifed member", value="rola przyznawana przez administracje dla zaufanych osob", inline=False)
+	embed.set_footer(text='W razie problemow prosimy o kontakt z administracja')
+	embed.set_author(name='Makro',
+	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/487981250108194816/zmxn.png')
+	embed.add_field(name="member", value="Rola dla osob ktore osiagnely lvl 5", inline=False)
+	embed.add_field(name="Active member", value="Rola dla aktywnych osob (+10lvl)", inline=False)
+	embed.add_field(name="verifed member", value="rola przyznawana przez administracje dla zaufanych osob", inline=False)
 
-#	await client.say(embed=embed)
-#
-#@client.command(name='regulamin',
-#				description="regulamin",
-#				brief="aktualny regulamin serwera (nie też nie tego serwera)",
-#				pass_context=True)
-#async def regulamin():
-#	embed = discord.Embed (
-#		title= 'REGULAMIN',
-#		description= 'Serwer jest publicznym serwerem z tego powodu prosimy zachowac kulture osobista',
-#		color = discord.Color.blue()
-#	)
+	await client.say(embed=embed)
 
-#	embed.set_footer(text='W razie problemow prosimy o kontakt z administracja')
-#	embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/485045416757428244/487981245826072577/XD.png')
-#	embed.set_author(name='Makro',
-#	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/487981250108194816/zmxn.png')
-#	embed.add_field(name="zakaz obrazania", value="Prosimy nie obrazac innych uzytkownikow", inline=False)
-#	embed.add_field(name="zakaz kontentu NSFW", value="Prosimy nie udostepniac tresci pornograficznych itp, itd", inline=False)
-#	embed.add_field(name="zakaz reklamy", value="Prosimy nie reklamowac sie na serwerze", inline=False)
+@client.command(name='regulamin',
+				description="regulamin",
+				brief="aktualny regulamin serwera (nie też nie tego serwera)",
+				pass_context=True)
+async def regulamin():
+	embed = discord.Embed (
+		title= 'REGULAMIN',
+		description= 'Serwer jest publicznym serwerem z tego powodu prosimy zachowac kulture osobista',
+		color = discord.Color.blue()
+	)
 
-#	await client.say(embed=embed)
+	embed.set_footer(text='W razie problemow prosimy o kontakt z administracja')
+	embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/485045416757428244/487981245826072577/XD.png')
+	embed.set_author(name='Makro',
+	icon_url='https://cdn.discordapp.com/attachments/485045416757428244/487981250108194816/zmxn.png')
+	embed.add_field(name="zakaz obrazania", value="Prosimy nie obrazac innych uzytkownikow", inline=False)
+	embed.add_field(name="zakaz kontentu NSFW", value="Prosimy nie udostepniac tresci pornograficznych itp, itd", inline=False)
+	embed.add_field(name="zakaz reklamy", value="Prosimy nie reklamowac sie na serwerze", inline=False)
+
+	await client.say(embed=embed)
 	
 	
 newUserMessage =  """ **WITAJ**
